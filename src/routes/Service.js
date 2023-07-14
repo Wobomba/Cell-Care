a263718074ee8dea2117b78077206cc854bcea98
Isaac Newton <iwobomba999@gmail.com>
2023-07-14 10:01:35 +0000

    card changes

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -1,14 +1,9 @@
 import Hero from '../components/Hero';
 import Navbar from "../components/Navbar";
 import AboutImg from '../assets/1.jpg';
-import Trip from '../components/Trip';
+import ServicesTripTrip from '../components/ServicesTrip';
 import Footer from '../components/Footer';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import hey from "../assets/Exp1.jpeg";
+
 
 function Service() {
     return (
@@ -20,29 +15,10 @@ function Service() {
                 title='Service'
                 btnClass='hide'
             />
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={hey}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Lizard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
+            
 
 
-
-            <Trip />
+            <ServicesTripTrip/>
             <Footer />
         </>
     )
