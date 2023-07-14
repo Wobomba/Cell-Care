25668bd480a7699af94f28fb2e3fe0110b5957f1
Isaac Newton <iwobomba999@gmail.com>
2023-07-14 08:05:10 +0000

    all merge pass

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -45,34 +45,7 @@ function Trip(){
                  every moment is filled with awe-inspiring discoveries."/>
 
                 
-                <TripData 
-                image={Trip2}
-                heading="The Coast of Mombasa"
-                text="                
-                Experience the epitome of bliss as you embark on a captivating coastal safari along the stunning shores of 
-                Mombasa. Discover a world where turquoise waters caress pristine white sands, 
-                and palm trees sway in the gentle ocean breeze. Immerse yourself in the vibrant culture and 
-                rich history of this enchanting coastal paradise. Explore lush mangrove forests, encounter exotic marine 
-                life, and indulge in tantalizing local cuisine. From thrilling water sports to serene beachside relaxation,
-                a coastal safari in Mombasa offers an unrivaled fusion of adventure and tranquility.
-                Get ready to be swept away by the beauty of this coastal gem and create 
-                cherished memories that will linger in your heart forever"
-                />
-                <TripData 
-                image={Trip3}
-                heading="Nature walks in Mabira Forest"
-                text="                
-                Venture into the lush embrace of Mabira Forest, where nature's symphony awaits.
-                 Lose yourself in a captivating journey through towering canopies, where sunlight dances upon the emerald foliage.
-                  Every step unveils a treasure trove of biodiversity, with rare birds serenading your senses and curious monkeys 
-                  peeking through the branches. Immerse yourself in the forest's serenity as you stroll along enchanted pathways, 
-                  guided by the gentle rustling of leaves and the fragrance of wildflowers. Engage with knowledgeable guides who 
-                  unlock the secrets of this enchanting ecosystem, sharing tales of ancient trees and medicinal plants. 
-                  Let the forest's tranquility wash over you, rejuvenating your spirit and leaving you with memories that 
-                  whisper of a profound connection to Mother Earth. Embark on a nature walk in Mabira Forest,
-                 where the wonders of the wild come alive in every step you take."
-
-                />
+              
             </div>
         </div>
     );
