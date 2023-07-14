8bbd4d3f34d29a59ded9cb34e4c948b884d76892
Isaac Newton <iwobomba999@gmail.com>
2023-07-14 10:06:56 +0000

    changes

---
+++ src/components/ServicesTrip.js
@@ -0,0 +1,63 @@
+import './ServicesTripStyles';
+import TripData from './ServicesTripData'; 
+
+import Trip1 from '../assets/Exp4.jpeg';
+import Trip2 from '../assets/Exp61.jpeg';
+import Trip3 from '../assets/mabira-trek.jpg';
+
+
+function ServicesTrip(){
+    return(
+        <div className='trip'>
+
+
+            <h1>Recent Trips</h1>
+            <p>Discover the untamed beauty of the African Savannah with our thrilling safari excursions.</p>
+
+            <div className='tripcard'>
+                <TripData 
+                image={Trip1}
+                heading="Safari Packages"
+                />
+                <TripData 
+                image={Trip2}
+                heading="Private Trips"
+                />
+                <TripData 
+                image={Trip3}
+                heading="Honeymoon Packages"/>
+            </div>
+
+            <div className='tripcard'>
+                <TripData 
+                image={Trip1}
+                heading="Tour Guiding"
+                />
+                <TripData 
+                image={Trip2}
+                heading="Retreats"
+                />
+                <TripData 
+                image={Trip3}
+                heading="Airport Drops"/>
+            </div>
+
+            <div className='tripcard'>
+                <TripData 
+                image={Trip1}
+                heading="Events Planning"
+                />
+                <TripData 
+                image={Trip2}
+                heading="Photography"
+                />
+                <TripData 
+                image={Trip3}
+                heading="Tents Booking"/>
+            </div>
+
+        </div>
+    );
+}
+
+export default ServicesTrip;
