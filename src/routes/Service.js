86bded1d3c60c712c85a0594e5d35659f38141be
Isaac Newton <iwobomba999@gmail.com>
2023-07-14 08:29:37 +0000

    pass

--- src/routes/Service.js
+++ src/routes/Service.js
@@ -3,19 +3,46 @@ import Navbar from "../components/Navbar";
 import AboutImg from '../assets/1.jpg';
 import Trip from '../components/Trip';
 import Footer from '../components/Footer';
+import Card from '../components/Card.jsx';
+import img1 from "../src/assets/Exp1.jpeg";
+import img2 from "../src/assets/Exp2.jpeg";
+import img3 from "../src/assets/Exp3.jpeg";
+import img4 from "../src/assets/Exp4.jpeg";
+import img5 from "../src/assets/Exp55.jpeg";
+import img6 from "../src/assets/Exp56.jpeg";
 
 function Service() {
     return (
         <>
-          <Navbar />
+            <Navbar />
             <Hero
                 cName='hero-mid'
                 heroImg={AboutImg}
                 title='Service'
                 btnClass='hide'
             />
-            <Trip/>
-            <Footer/>
+
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-4">
+                            <Card title="Safari Packages" imgsrc={img1} />
+                            <Card title="Private Trips" imgsrc={img2} />
+                            <Card title="Honey Moon Packages" imgsrc={img3} />
+                            <Card title="Tour Guiding" imgsrc={img4} />
+                            <Card title="Retreats" imgsrc={img5} />
+                            <Card title="Airport Pickups and Drops" imgsrc={img6} />
+                            <Card title="Hotel Bookings" imgsrc={img6} />
+                            <Card title="Events Planning" imgsrc={img6} />
+                            <Card title="Photography" imgsrc={img6} />
+                            <Card title="Camp Tent Booking" imgsrc={img6} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Trip />
+            
         </>
     )
 }
