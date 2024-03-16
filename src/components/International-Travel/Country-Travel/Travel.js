1661e3ea6ec3ef37d82f85747538ad5b73469ea4
Isaac Newton <iwobomba999@gmail.com>
2024-03-16 10:14:37 +0000

    almost-done3

--- src/components/International-Travel/Country-Travel/Travel.js
+++ src/components/International-Travel/Country-Travel/Travel.js
@@ -4,18 +4,22 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './TravelStyles.css';
 import TravelData from './TravelData';
-import Mountain1 from '../../../assets/bungee.jpg';
-import Mountain2 from '../../../assets/3_latest.jpg';
-import Mountain3 from '../../../assets/4_latest.jpg';
+import Ug from '../../../assets/KDP_9483.jpg';
+import Ke from '../../../assets/KDP_9504.jpg';
+import Tz from '../../../assets/KDP_0099.jpg';
+import Zn from '../../../assets/9_latest.jpg';
+import Sa from '../../../assets/sa-1.jpg';
+import Di from '../../../assets/8_latest.jpg';
+
 
 const Travel = () => {
   const countries = [
-    { name: 'Uganda', image: Mountain1, link: '/uganda' },
-    { name: 'Kenya', image: Mountain2, link: '/kenya' },
-    { name: 'Tanzania', image: Mountain3, link: '/tanzania' },
-    { name: 'Zanzibar', image: Mountain3, link: '/zanzibar' },
-    { name: 'Dubai', image: Mountain3, link: '/dubai' },
-    { name: 'South Africa', image: Mountain3, link: '/south-africa' },
+    { name: 'Uganda', image: Ug, link: '/uganda' },
+    { name: 'Kenya', image: Ke, link: '/kenya' },
+    { name: 'Tanzania', image: Tz, link: '/tanzania' },
+    { name: 'Zanzibar', image: Zn, link: '/zanzibar' },
+    { name: 'Dubai', image: Di, link: '/dubai' },
+    { name: 'South Africa', image: Sa, link: '/south-africa' },
   ];
 
   return (
